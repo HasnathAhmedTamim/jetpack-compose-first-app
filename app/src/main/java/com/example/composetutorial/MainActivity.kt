1c4af2ac5dddc1189f3d0cc6338938bc38485721
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-01-06 12:16:57 +0600

    first compose app

---
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+data class Message(val author: String, val body: String)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard(Message("Android", "Jetpack Compose"))
+        }
+    }
+}
+
+@Composable
+fun MessageCard(msg: Message) {
+    Column {
+        Text(text = msg.author)
+        Text(text = msg.body)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Sarah", "Hey! Check out Jetpack Compose")
+    )
+}
